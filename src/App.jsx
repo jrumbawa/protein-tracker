b4d7c4335f817309d5d7b0106e773474248c09b4
Jomar Rumbawa <rumbawajomar@gmail.com>
2023-02-28 12:29:33 -0800

    Add form functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import { useState } from 'react'
 
 function App() {
-  return <h1>Protein Tracker</h1>
+  const [proteins, setProteins] = useState([])
+  const [food, setFood] = useState('')
+  const [grams, setGrams] = useState(0)
+
+  const addProtein = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+      <h1>Protein Tracker</h1>
+      <form onSubmit={addProtein}>
+        Food: <input value={food} onChange={(e) => setFood(e.target.value)} />
+        <br />
+        Protein (grams):{' '}
+        <input type="number" value={grams} onChange={(e) => setGrams(e.target.value)} />
+        <br />
+        <button>Add</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
