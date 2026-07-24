c5545f6935076a1e9c6ac3ca9aa0e01ebb03ec9d
Jomar Rumbawa <dev@jomarrumbawa.com>
2026-07-24 11:07:03 -0700

    feat: add data layer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Log from './components/Log'
+import ProteinInput from './components/ProteinInput'
 
 function App() {
-  return
+  const [entries, setEntries] = useState(() => {
+    const log = localStorage.getItem('entries')
+    return log ? JSON.parse(log) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem('entries', JSON.stringify(entries))
+  }, [entries])
+
+  function handleAddEntry(name, proteinAmount) {
+    const newEntry = {
+      id: crypto.randomUUID(),
+      name,
+      proteinAmount: parseInt(proteinAmount),
+      timestamp: Date.now(),
+    }
+
+    setEntries((prev) => [...prev, newEntry])
+  }
+
+  return (
+    <>
+      <ProteinInput onAddEntry={handleAddEntry} />
+      <Log entries={entries} />
+    </>
+  )
 }
 
 export default App
